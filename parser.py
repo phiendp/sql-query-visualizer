f4cfb1d52ff608bc56585672bbe9b2c13f87a68e
Phien Pham <phien@triip.me>
2017-12-27 01:23:33 +0700

    Parse the relationship between tables in the query

--- parser.py
+++ parser.py
@@ -1,7 +1,62 @@
-from pg_query import prettify
+from pg_query import Node, parse_sql
+import json
 
 
 class Parser(object):
 
+    def update_target_list(self, target_list, name, storage):
+        for target in target_list:
+            if target.val.node_tag == 'ColumnRef':
+                storage[name]['fields'].append(target.val.fields[-1].str.value)
+            elif target.val.node_tag == 'FuncCall':
+                storage[name]['fields'].append(target.name.value)
+
+    def build_ctes_from(self, cte_expression):
+        ctes = dict()
+        for cte in cte_expression:
+            name = cte.ctename.value
+            ctes[name] = dict()
+            ctes[name]['name'] = name
+            ctes[name]['children'] = []
+            # ctes[name]['fields'] = []
+
+            rel_expression = cte.ctequery.fromClause
+            for rel in rel_expression:
+                ctes[name]['children'].append(rel.relname.value)
+
+            # target_list = cte.ctequery.targetList
+            # self.update_target_list(target_list, name, ctes)
+        return ctes
+
+    def build_result_from(self, rel_result, target_list, ctes):
+        result = dict()
+        result['name'] = 'result'
+        result['children'] = []
+        # result['fields'] = []
+
+        for rel in rel_result:
+            name = rel.relname.value
+            if name not in ctes:
+                result['children'].append(name)
+            else:
+                result['children'].append(ctes[name])
+
+        # for target in target_list:
+        #     if target.val.node_tag == 'ColumnRef':
+        #         result['fields'].append(target.val.fields[-1].str.value)
+        #     elif target.val.node_tag == 'FuncCall':
+        #         result['fields'].append(target.name.value)
+        return result
+
     def process(self, sql):
-        return prettify(sql)
+        root = Node(parse_sql(sql))
+        # for node in root.traverse():
+        #     print(node)
+
+        cte_expression = root[0].stmt.withClause.ctes
+        ctes = self.build_ctes_from(cte_expression)
+
+        rel_result = root[0].stmt.fromClause
+        target_list = root[0].stmt.targetList
+        result = self.build_result_from(rel_result, target_list, ctes)
+        return json.dumps(result)
---
+++ tests.py
@@ -0,0 +1,72 @@
+import unittest
+from parser import Parser
+
+
+class QueryParserTests(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = Parser()
+
+    def test_parse_simple_query_with_one_cte(self):
+        query = """
+            WITH user_orders AS (
+              select
+              user_id,
+              count(1) as order_count
+              from orders O
+              where O.created_at > NOW() - INTERVAL '30 days'
+              group by 1
+            )
+            select
+              order_count,
+              count(1) as num_users
+            from user_orders
+            group by 1
+            order by 1
+        """
+
+        result = self.parser.process(query)
+        expected = '{"name": "result", "children": [{"name": "user_orders", "children": ["orders"]}]}'
+
+        assert result == expected
+
+    def test_parse_query_with_multiple_ctes(self):
+        query = """
+            with daily_users as (
+              select
+                created_at::date as date_d,
+                count(1) as new_users
+              from users
+              group by 1
+            ), daily_products as (
+              select
+                created_at::date as date_d,
+                count(1) as new_users
+              from products
+              group by 1
+            ), daily_reviews as (
+              select
+                created_at::date as date_d,
+                count(1) as new_users
+              from reviews
+              group by 1
+            )
+            select
+              U.date_d,
+              U.new_users,
+              P.new_products,
+              R.new_reviews
+            from daily_users U, daily_products P, daily_reviews R
+            where U.date_d = P.date_d
+              and U.date_d = R.date_d
+            order by 1 DESC
+        """
+
+        result = self.parser.process(query)
+        expected = '{"name": "result", "children": [{"name": "daily_users", "children": ["users"]}, {"name": "daily_products", "children": ["products"]}, {"name": "daily_reviews", "children": ["reviews"]}]}'
+
+        assert result == expected
+
+
+if __name__ == '__main__':
+    unittest.main()

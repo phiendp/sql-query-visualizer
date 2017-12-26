93622d9c485e3ef129793ce48e6c1f3906bd087a
Phien Pham <phien@triip.me>
2017-12-26 14:15:49 +0700

    Setup index.html for the app's main page

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from pg_query import prettify
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return "Hello World!"
+    result = ''
+    if request.method == "POST":
+        snippet = request.form['snippet']
+        result = prettify(snippet)
+    return render_template('index.html', results=result)
 
 
 if __name__ == '__main__':
